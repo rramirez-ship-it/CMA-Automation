7ef184734510817e9249a8465a521f9c58650b94
rramirez-ship-it <rramirez-ship-it@users.noreply.github.com>
2026-04-08 23:39:29 +0000

    deploy: 6aebc55e7225f5d1fcd6715bf73fae2f7eac5d21

--- automation/staging/page_object/home_dashboard.py
+++
@@ -1,9 +0,0 @@
-
-
-class homePage:
-    def __init__(self, page):
-        self.page = page
-        
-    def go_to_home_dashboard(self):
-        self.page.goto('https://platform.stg.lwolf.com/product/select?a=cc')
-        
--- automation/staging/page_object/user_menu.py
+++
@@ -1,7 +0,0 @@
-
-class user_menu:
-    def __init__(self, page):
-        self.page = page
-
-    def click_user_menu(self):
-        self.page.get_by_role("button", name="User menu").click()
